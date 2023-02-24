0103ea9284735928cab4c0f6382a8e0b3eb81a64
ecossistema.dev <contato@ecossistema.dev>
2023-02-24 10:19:08 -0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function mudarCor(){
+    let corRgb = `rgb(${Math.round(Math.random() * 255)} , ${Math.round(Math.random() * 255)} , ${Math.round(Math.random() * 255)})`;
+    document.body.style.backgroundColor = corRgb;
+};
+
+//Curso #30ProjetosJavaScript da Estudante.dev
